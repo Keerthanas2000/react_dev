973db103fdbe695a0cb445fc4ae8d2c51025094d
keerthana s <keerthanasbhardwaj@gmail.com>
2025-04-06 23:30:24 +0530

    use state changes

--- src/pages/Hooks/ALLHOOKS/usestate.js
+++ src/pages/Hooks/ALLHOOKS/usestate.js
@@ -16,13 +16,7 @@ function UsestateExample() {
 export default UsestateExample;
 
 
-// asynchronous state update
-// console.log will not update immediately
-//overwriting insted of updating 
 
-// setUser({name:"Keerthana"})
-
-// use setUser(prev=>({...prev, name:"Keerthana"}))
 
 //simple use case
 // toggle button
@@ -43,4 +37,39 @@ export default UsestateExample;
 // const [show, setShow] = useState(false);
 // <button onClick={() => setShow(!show)}>Toggle</button>
 // {show && <p>This is visible now!</p>}
+
+// `useState` returns an array: `[state, setState]`
+// An array with current state and a function to update it
+
+//// asynchronous state update
+// console.log will not update immediately
+//overwriting insted of updating 
+
+// setUser({name:"Keerthana"})
+// You must copy the previous object using spread syntax to avoid losing other properties. 
+// use setUser(prev=>({...prev, name:"Keerthana"}))
+
+// best practise to do heavy computations using usestate
+
+
+// When you write:
+
+
+// const [value, setValue] = useState(heavyFunction());
+//  The heavyFunction() gets executed every time the component renders — even if the state doesn’t change.
+
+// That’s bad for performance, especially if heavyFunction is slow.
+
+// ✅ The Right Way (Lazy Initialization):
+
+// const [value, setValue] = useState(() => heavyFunction());
+// In this form:
+
+// You're passing a function, not its result.
+
+// React calls your function only once, during the initial render.
+
+// This saves performance because it doesn’t recompute on every render.
+
+
 
